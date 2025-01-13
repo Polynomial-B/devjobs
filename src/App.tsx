c37e4437c9f4aa447b9ade3c0fea6c99cb690334
Matt <mphlamb@gmail.com>
2025-01-13 12:07:52 +0100

    state lifted

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
+import { useEffect, useState } from "react";
 import Background from "./components/Background";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
+	const [jobItems, setJobItems] = useState([]);
+	const [searchText, setSearchText] = useState("");
+
+	useEffect(() => {
+		if (!searchText) return;
+
+		const fetchData = async () => {
+			const res = await fetch(
+				`https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+			);
+			const data = await res.json();
+			setJobItems(data.jobItems);
+		};
+		fetchData();
+	}, [searchText]);
+
 	return (
 		<>
 			<Background />
-			<Header />
-			<Container />
+			<Header searchText={searchText} setSearchText={setSearchText} />
+			<Container jobItems={jobItems} />
 			<Footer />
 		</>
 	);
