cf6ff6230f43ef72a673a0a95809d586a54fe66f
Matt <mphlamb@gmail.com>
2025-01-23 14:00:41 +0100

    useJobItems hook added

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Background from "./components/Background";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { useJobItems } from "./lib/hooks";
 
 function App() {
-	const [jobItems, setJobItems] = useState([]);
 	const [searchText, setSearchText] = useState("");
-	const [isLoading, setIsLoading] = useState(false);
-
-	useEffect(() => {
-		if (!searchText) return;
-
-		const fetchData = async () => {
-			setIsLoading(true);
-			const res = await fetch(
-				`https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
-			);
-			const data = await res.json();
-			setIsLoading(false);
-			setJobItems(data.jobItems);
-		};
-		fetchData();
-	}, [searchText]);
-
+	const { jobItems, isLoading } = useJobItems(searchText);
 	return (
 		<>
 			<Background />
