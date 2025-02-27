b75513e7d2c48366ef32f6f7270cec9b8dbb45b8
Matt <mphlamb@gmail.com>
2025-02-27 17:08:39 +0100

    debounce added to hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Background from "./components/Background";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { useJobItems } from "./lib/hooks";
+import { useDebounce, useJobItems } from "./lib/hooks";
 
 function App() {
 	const [searchText, setSearchText] = useState("");
-	const [debouncedSearchText, setDebouncedSearchText] = useState("");
+	const debouncedSearchText = useDebounce(searchText, 300);
 	const { jobItems, isLoading, totalJobCount } =
 		useJobItems(debouncedSearchText);
-
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setDebouncedSearchText(searchText);
-		}, 600);
-		return () => clearTimeout(timer);
-	}, [searchText]);
 
 	return (
 		<>
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -61,3 +61,15 @@ export function useDisplayedItem(paramId: number | null) {
   }, [paramId]);
   return [displayedItem, isLoading] as const;
 }
+
+export function useDebounce<T>(value: T, delay = 500): T {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
