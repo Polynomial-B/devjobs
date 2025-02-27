f55f934936508b752dd81a03d1d61f970da2d1a9
Matt <mphlamb@gmail.com>
2025-02-27 16:04:16 +0100

    update

--- src/components/ResultsCount.tsx
+++ src/components/ResultsCount.tsx
@@ -1,5 +1,9 @@
 import { ContainerProps } from "../lib/types";
 
 export default function ResultsCount({ totalJobCount }: ContainerProps) {
-	return <p className="count">{totalJobCount} results</p>;
+	return (
+		<p className="count">
+			<span className="u-bold">{totalJobCount}</span> results
+		</p>
+	);
 }
