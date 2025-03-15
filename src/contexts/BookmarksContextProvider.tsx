90f2864b854681e74b43482dbbb8af7cd8168439
Matt <mphlamb@gmail.com>
2025-03-15 12:39:03 +0100

    localstorage save/get added

--- src/contexts/BookmarksContextProvider.tsx
+++ src/contexts/BookmarksContextProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { BookmarksContext } from "./BookmarksContext";
 
 export default function BookmarksContextProvider({
@@ -6,7 +6,9 @@ export default function BookmarksContextProvider({
 }: {
 	children: ReactNode;
 }) {
-	const [bookmarkedIDs, setBookmarkedIDs] = useState<number[]>([]);
+	const [bookmarkedIDs, setBookmarkedIDs] = useState<number[]>(() =>
+		JSON.parse(localStorage.getItem("bookmarkedIDs") || "[]")
+	);
 	const handleToggleBookmark = (id: number) => {
 		if (bookmarkedIDs.includes(id)) {
 			setBookmarkedIDs((prev) => prev.filter((item) => item !== id));
@@ -14,6 +16,11 @@ export default function BookmarksContextProvider({
 			setBookmarkedIDs((prev) => [...prev, id]);
 		}
 	};
+
+	useEffect(() => {
+		localStorage.setItem("bookmarkedIDs", JSON.stringify(bookmarkedIDs));
+	}, [bookmarkedIDs]);
+
 	return (
 		<BookmarksContext.Provider
 			value={{ bookmarkedIDs, handleToggleBookmark }}
